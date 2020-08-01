9d23add0f0afa4630c0c884d3a2cdd9e7538b4c8
Sneha95 <sneha.alex@attinadsoftware.com>
2020-08-01 21:14:02 +0530

    Updates

---
+++ precache-manifest.b0a3ece83e29b47fb84b0e7e32fe576c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7559a4d13a11354cde46e9116549d2",
+    "url": "/newsBuzz/index.html"
+  },
+  {
+    "revision": "63e6c9e3107316a39a7f",
+    "url": "/newsBuzz/static/css/main.09d813cc.chunk.css"
+  },
+  {
+    "revision": "2955c5b02454d9c35291",
+    "url": "/newsBuzz/static/js/2.da340b6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newsBuzz/static/js/2.da340b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e6c9e3107316a39a7f",
+    "url": "/newsBuzz/static/js/main.773f7402.chunk.js"
+  },
+  {
+    "revision": "ed49ac9f01c7c7e4a60d",
+    "url": "/newsBuzz/static/js/runtime-main.8d75da30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newsBuzz/static/media/logo.5d5d9eef.svg"
+  }
+]);
